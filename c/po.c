36301a7e5c1c74d98df041f97ada233a2b8cf203
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-01-11 10:48:46 +0530

    Create po.c

---
+++ c/po.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  double num=455;
+  printf("enter the values");
+
+if(1<=num<=100000)
+{
+    if(num==100000)
+        printf("you enter the same number");
+     else
+        printf("you enter the new number");
+}
+else
+   printf("you enter the positive number");
+return 0;
+}
+ 
+
+
