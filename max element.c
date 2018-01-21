ac78200e0d60b60333069cc0a58abe25042b2a61
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-01-21 12:17:20 +0530

    Create max element.c

---
+++ max element.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int array[100], maximum, size=80, c=90, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+ 
+  printf("Enter %d integers\n", size);
+ 
+  for (c = 0; c < size; c++)
+ 
+  maximum = array[0];
+ 
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+    {
+       maximum  = array[c];
+       location = c+1;
+    }
+  }
+ 
+  printf("Maximum element is present at location %d and it's value is %d.\n", location, maximum);
+  return 0;
+}
