61119c37d232d196aeb7114b4d0eed3c7fad9555
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-01-24 15:56:04 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,p=7,median,sum=0,a[i];
+for(i=0;i<p;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+median=sum/p;
+printf("THE MEDIAN VALUE OF THE ARRAY",median);
+return 0;
+}
