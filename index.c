3accb716c1299b21c745e64913446380c1a128c7
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-01-24 16:16:43 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[20],i,t;
+printf("Enter the size of array");
+scanf("%d",&a[i]);
+printf("Enter the elements");
+scanf("%d",&t);
+for(i=0;i<t;i++);
+{
+printf("%d%d",a[i],i);
+}
+}
