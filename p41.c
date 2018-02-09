6e440fcfe6be7c11dd8ebca228dfa069405ec2d9
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-02-09 16:06:42 +0530

    Create p41.c

---
+++ p41.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char j[100];
+    printf("enter the string\n");
+    scanf("\n%s%d",&j);
+    for(i=1;i<=10;i++)
+    {
+        printf("\n%s",j);
+    }
+    return 0;
+}
