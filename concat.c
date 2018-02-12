183a23250e46d8c000a3631d2a67c97838a91f0a
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-02-12 16:36:12 +0530

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(void)
+{
+  char str1[25],str2[25];
+  int m=0,n=0;
+  printf("\nEnter First String:");
+  gets(str1);
+  printf("\nEnter Second String:");
+  gets(str2);
+  while(str1[m]!='\0')
+  i++;
+  while(str2[n]!='\0')
+  {
+    str1[m]=str2[n];
+    m++;
+    n++;
+  }
+  str1[m]='\0';
+  printf("\nConcatenated String is %s",str1);
+}
