c704abdc8b2440ef1504d719d3b8cbac927f2351
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-02-19 15:57:49 +0530

    Create p44.c

---
+++ p44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int j,n;
+    printf("enter the no");
+    scanf("%d",&j);
+    for(n=1;n<=10;n++)
+    {
+        if(j<=10)
+        {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    }
+}
