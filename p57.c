4f3bd7e272ba1c674fe5f62cf17f4472df0516fc
aravindhad <35321941+aravindhad@users.noreply.github.com>
2018-03-12 16:31:12 +0530

    Create p57.c

---
+++ p57.c
@@ -0,0 +1,19 @@
+#include<include>
+#include<conio.h>
+void swap(int a,int b)
+void main()
+{
+int a=10,b=20;
+clrscr();
+printf("enter the numbers\t%d\t%d",a,b);
+swap(a,b);
+}
+void swap(int x,int y)
+{
+int c;
+x=c;
+y=x;
+c=y;
+printf("After swaping\t%d\t%d",x,y);
+getch();
+}
